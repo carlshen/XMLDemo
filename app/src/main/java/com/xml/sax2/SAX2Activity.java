a85d3c1b4a4063658ab2fa7145ed48005326d503
xiangfeng shen <xiangfeng.shen@outlook.com>
2022-11-19 13:58:03 +0800

    upgrade to androidx, sdk30 for use

--- app/src/main/java/com/xml/sax2/SAX2Activity.java
+++ app/src/main/java/com/xml/sax2/SAX2Activity.java
@@ -2,7 +2,7 @@ package com.xml.sax2;
 
 import android.content.res.AssetManager;
 import android.os.Bundle;
-import android.support.v7.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatActivity;
 import android.view.View;
 import android.widget.TextView;
 
